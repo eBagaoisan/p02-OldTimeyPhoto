3cbfd0b3c1263fe10fd4f23bdab1cfcd392d00c0
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2017-10-02 17:28:14 -0700

    Functional Decomposition

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
+#include <vector>
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main(){
+    //variables
+    //vector initialization
+
+    //Introduction of program to user
+
+    //Allow user to access file of an image
+
+    //check "if" image fits pixel type; if not loop back to intro
+    
+    //convert image pixel into vector matrix
+    //change pixel values to gray scale values using math cmath to get average
+
+    //Save image grayscale as a new file
+
+
+    return 0;
+}
