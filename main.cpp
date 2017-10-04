21fa1bc70ed71e747cff2b05b727402517d60b20
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2017-10-04 16:30:56 -0700

    Added Bitmap type variable and Pixel type vector to create matrix of Machupicchu image.

--- main.cpp
+++ main.cpp
@@ -7,14 +7,20 @@ using namespace std;
 
 int main(){
     //variables
-    //vector initialization
+    Bitmap image;
+    vector<vector<Pixel> > bmp;
+    Pixel rgb;
+
+    //Example Print
+    bmp = image.toPixelMatrix();
+    image.open("machupicchu.bmp");
 
     //Introduction of program to user
 
     //Allow user to access file of an image
 
     //check "if" image fits pixel type; if not loop back to intro
-    
+
     //convert image pixel into vector matrix
     //change pixel values to gray scale values using math cmath to get average
 
