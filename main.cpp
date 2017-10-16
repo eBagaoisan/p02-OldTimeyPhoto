69e3ed358c82f85e8b6158a8911f450d612874a0
ebagaoisan <ebagaoisan1@mail.csuchico.edu>
2017-10-15 19:04:38 -0700

    Added loop and if statement to check if image entered by user is true

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ int main(){
     int row;
     int column;
     string file;
+    bool realImage = true;
 
     //Project Planning part 2 example matrix
     /*
@@ -23,35 +24,55 @@ int main(){
     */
 
     //Allow user to access file of an image
-    cout<<"Please enter a bmp image file that you would like to be gray scaled(filename.bmp): ";
-    cin>>file;
+    while( realImage = true){
+        cout<<"Please enter a bmp image file that you would like to be gray scaled(filename.bmp): ";
+        cin>>file;
 
-    //check "if" image fits pixel type; if not loop back to intro
-    image.open(file);
+    //checks "if" image fits pixel type; if not loop back to intro
+        image.open(file);
+        realImage = image.isImage();
 
+    if (realImage = true){
     //convert image pixel into vector matrix
-    bmp = image.toPixelMatrix();
+        bmp = image.toPixelMatrix();
     
     //change pixel values to gray scale values using math cmath to get average
-    for (int r = 0; r<bmp.size(); r++)
-    {
-        for (int c = 0; c<bmp[r].size(); c++)
+        for (int r = 0; r<bmp.size(); r++)
         {
-            rgb = bmp[r][c];
-            rgbAvg = (rgb.red + rgb.green + rgb.blue)/3;
-            rgb.red = rgbAvg;
-            rgb.green = rgbAvg;
-            rgb.blue = rgbAvg;
-            bmp[r][c] = rgb;
+            for (int c = 0; c<bmp[r].size(); c++)
+            {
+                rgb = bmp[r][c];
+                rgbAvg = (rgb.red + rgb.green + rgb.blue)/3;
+                rgb.red = rgbAvg;
+                rgb.green = rgbAvg;
+                rgb.blue = rgbAvg;
+                bmp[r][c] = rgb;
+            }
         }
-    }
-    image.fromPixelMatrix(bmp);
+        image.fromPixelMatrix(bmp);
     
-    cout<<"Image size: "<<bmp[0].size()<<"X"<<bmp.size()<<"pixels"<<endl;
+        cout<<"Image size: "<<bmp[0].size()<<"X"<<bmp.size()<<"pixels"<<endl;
 
     //Save image grayscale as a new file
-    image.save("new"+file);
-    cout<<"New image saved as: new"+file<<endl;
+        image.save("new"+file);
+        cout<<"New image saved as: new"+file<<endl;
+        break;
+    }
+    else{
+        cout<<"Not an image... Try again."<<endl;
+
+    }
+    }
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
